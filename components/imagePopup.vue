1775ebb5ab2fa5dab6bb3294d503f15cc81e68c0
saurluca <spuckteller@gmail.com>
2024-09-05 11:48:28 +0200

    increased size of closing button

--- components/imagePopup.vue
+++ components/imagePopup.vue
@@ -41,7 +41,11 @@ defineExpose({ open, close, setImageBlob })
 <template>
   <div v-if="isOpen" class="fixed inset-0 z-50 flex items-center justify-center p-4 bg-black bg-opacity-50">
     <div class="relative max-w-3xl w-full bg-white dark:bg-gray-800 rounded-lg shadow-xl">
-      <button @click="close" class="absolute top-2 right-2 text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200">
+      <button 
+        @click="close" 
+        class="absolute -top-3 -right-3 bg-gray-200 dark:bg-gray-700 text-gray-700 dark:text-gray-300 hover:bg-gray-300 dark:hover:bg-gray-600 rounded-full p-2 transition-colors duration-200"
+        aria-label="Close"
+      >
         <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
         </svg>
