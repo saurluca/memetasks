ac16c04d99768609851ab90a1f031fb1b2e55291
saurluca <spuckteller@gmail.com>
2024-09-19 13:28:40 +0200

    Revert "changed seo stuff"
    This reverts commit 5b9e62dc

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -36,9 +36,50 @@ export default defineNuxtConfig({
       title: 'Memetasks',
       meta: [
         {
+          hid: 'description',
           name: 'description',
           content: 'Memetasks is a fun and simple to-do app where checking off tasks rewards you with AI-generated memes tailored ' +
-              'to what you’ve completed.',
+              'to what you’ve completed. Fast, offline-first, and designed for a clean, fuss-free experience.',
+        },
+        {
+          hid: 'keywords',
+          name: 'keywords',
+          content: 'to-do app, meme app, productivity, AI-generated memes, offline-first, simple task manager'
+        },
+        {
+          hid: 'author',
+          name: 'author',
+          content: 'Memetasks Team'
+        },
+        {
+          hid: 'og:title',
+          property: 'og:title',
+          content: 'Memetasks - Meme-based To-Do App'
+        },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content: 'Complete tasks and get rewarded with fun AI-generated memes! Memetasks is fast, offline-first, and easy to use.'
+        },
+        {
+          hid: 'og:url',
+          property: 'og:url',
+          content: 'https://memetasks.com'
+        },
+        {
+          hid: 'twitter:card',
+          name: 'twitter:card',
+          content: 'summary_large_image'
+        },
+        {
+          hid: 'twitter:title',
+          name: 'twitter:title',
+          content: 'Memetasks- Your meme-based To-Do App'
+        },
+        {
+          hid: 'twitter:description',
+          name: 'twitter:description',
+          content: 'Get rewarded with fun memes when you complete tasks! Fast, offline, and simple.'
         },
       ]
 
