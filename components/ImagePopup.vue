21e53da2f1dde0058e1256650ce17820b49dcbfd
saurluca <spuckteller@gmail.com>
2024-09-26 00:21:15 +0200

    no header mode for testing

--- components/ImagePopup.vue
+++ components/ImagePopup.vue
@@ -43,7 +43,7 @@ defineExpose({ open, close, setImageBlob, resetImageBlob })
 
 <template>
   <div v-if="isOpen" class="fixed inset-0 z-50 flex items-center justify-center p-4 bg-black bg-opacity-50">
-    <div class="relative max-w-xl w-full bg-slate-100 dark:bg-gray-500 rounded-lg shadow-xl">
+    <div class="relative max-w-2xl w-full bg-slate-100 dark:bg-gray-500 rounded-lg shadow-xl">
       <button 
         @click="close" 
         class="absolute -top-3 -right-3 bg-gray-200 dark:bg-black text-gray-700 dark:text-gray-300 hover:bg-gray-300 dark:hover:bg-gray-600 rounded-full p-2 transition-colors duration-200"
@@ -55,7 +55,7 @@ defineExpose({ open, close, setImageBlob, resetImageBlob })
       </button>
       <div class="p-4">
         <img v-if="imageUrl" :src="imageUrl" alt="Todo image" class="w-full h-auto rounded-lg" />
-        <p v-else-if="!imageBlob" class="w-full h-auto rounded-lg text-gray-500">No image provided. <br/>Either you are checking your tasks to quickly, or there was an error while generating the image.</p>
+        <p v-else-if="!imageBlob" class="max-w-xl h-auto rounded-lg text-gray-500">No image provided. <br/>Either you don't give the memes enough time to fully grow, or there was an error while generating the image.</p>
         <p v-else class="text-gray-500">Loading image...</p>
       </div>
     </div>
--- composables/memeGenerator.ts
+++ composables/memeGenerator.ts
@@ -56,6 +56,7 @@ export function useMemeGenerator() {
             }
 
             const blob = await imageResponse.blob()
+            return blob
 
             // Create a canvas to draw the image and the meme header
             const canvas = document.createElement('canvas');
