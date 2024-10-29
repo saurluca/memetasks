35b21c07cbfb4e926ac81906da8edde7963a2bfc
saurluca <spuckteller@gmail.com>
2024-10-29 12:32:37 +0100

    fixed line clamp issues if task is one long string

--- components/TodoItem.vue
+++ components/TodoItem.vue
@@ -37,25 +37,25 @@ const handleCheckboxClick = (event, todo) => {
 
 <template>
   <li :key="todo.id" class="bg-slate-50 p-2 rounded-lg shadow-sm cursor-default hover:bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600" @click="handleExpandClick">
-    <div class="flex flex-row gap-2 justify-between">
-      <div class="flex gap-2 ml-1 justify-between w-full" >
-        <div class="flex gap-2">
-          <div class="mt-2.5 mr-1">
+    <div class="flex flex-row gap-2">
+      <div class="flex gap-2 ml-1 flex-1 min-w-0"> <!-- Changed to flex-1 to take available space but allow shrinking -->
+        <div class="flex gap-2 min-w-0 flex-1"> <!-- Added flex-1 to allow text to take remaining space -->
+          <div class="mt-2.5 mr-1 flex-shrink-0">
             <input type="checkbox" class="size-5 bg-green-500" :checked="todo.completed" @click="(event) => handleCheckboxClick(event, todo)"/>
           </div>
-          <div class="text-slate-800 pt-2 dark:text-slate-300" :class="{ 'line-through': todo.completed, 'opacity-50': todo.completed, 'line-clamp-1': !expandedText }">
+          <div class="text-slate-800 pt-2 dark:text-slate-300 overflow-hidden min-w-0" :class="{ 'line-through': todo.completed, 'opacity-50': todo.completed, 'truncate': !expandedText }">
             {{ todo.text }}
           </div>
         </div>
-        <div class="text-slate-500 pt-2 dark:text-slate-300 inline-block whitespace-nowrap" :class="{ 'line-through': todo.completed, 'opacity-50': todo.completed }">
+        <div class="text-slate-500 pt-2 dark:text-slate-300 inline-block whitespace-nowrap flex-shrink-0" :class="{ 'line-through': todo.completed, 'opacity-50': todo.completed }">
           {{ formattedDate }}
         </div>
       </div>
-      <div class="flex flex-col items-center gap-2 text-sm justify-between pt-1.5 pr-0.5">
+      <div class="flex flex-col items-end gap-2 text-sm justify-between pt-1.5 pr-0.5 flex-shrink-0">
         <div v-if="todo.tags" class="px-2 py-1 rounded-full whitespace-nowrap shadow-sm" :class="todo.tags && currentTag == todo.tags ? getTagColorActive(todo.tags) : getTagColorInactive(todo.tags)">
           {{ todo.tags }}
         </div>
-        <button class="text-red-700 bg-red-300 dark:hover:bg-red-500 rounded-full shadow-sm mb-0.5 p-1.5 ml-auto mt-auto hover:bg-red-500 hover:text-white" v-if="expandedText" @click="emit('delete-todo', todo.id)">
+        <button class="text-red-700 bg-red-300 dark:hover:bg-red-500 rounded-full shadow-sm mb-0.5 p-1.5 hover:bg-red-500 hover:text-white" v-if="expandedText" @click="emit('delete-todo', todo.id)">
           delete
         </button>
       </div>
