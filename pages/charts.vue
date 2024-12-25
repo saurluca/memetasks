0a1b3c30a9868129e8f4b6c1499e31a2b7c5b760
saurluca <spuckteller@gmail.com>
2024-12-25 20:26:03 +0100

    created charts page

---
+++ pages/charts.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const client = useSupabaseClient()
+const user = useSupabaseUser()
+
+</script>
+
+<template>
+  <d-page>
+    <div>
+      <h1 class="page-title">Life Charts</h1>
+    </div>
+    <div>
+      Chart 1
+    </div>
+  </d-page>
+</template>
+
+<style scoped>
+
+</style>
